35167404e3a4a10cd38eafbf28618724fb6f6538
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-19 18:41:44 -0500

    Add image of the day

--- js/main.js
+++ js/main.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
 	};
 	timeUpdFunc();
 	setInterval(timeUpdFunc, 1000);
+
+	$.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US", function(response) {
+		$("#section1").css("background-image", "url(https://www.bing.com" + response.images[0].url + ")");
+	})
 });
