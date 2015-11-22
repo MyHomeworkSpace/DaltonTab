78c191fe2d508164bc87372e81616b16896d1cfa
c20et <c20et@dalton.org>
2015-11-22 11:31:16 -0500

    THIS IS WILL - PARALLAX HAZ BIN HEXD
    ALL YOUR BASE ARE BELONG TO US

--- js/main.js
+++ js/main.js
@@ -21,9 +21,10 @@ $(document).ready(function() {
 
 	$.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US", function(response) {
 		var url = "https://www.bing.com" + response.images[0].url;
-		$("#section1").parallax({ imageSrc: url, bleed: 20, positionY: "0px" });
+		$("#par").parallax({ imageSrc: url, bleed: 20, positionY: "0px" });
+		$("#par").addClass("imageLoaded");
 		$("#section1").addClass("imageLoaded");
-		$("body").css("background-image", "url(" + url + ")");
+		//$("body").css("background-image", "url(" + url + ")");
 	});
 
 	$("#settingsBtn").click(function() {
