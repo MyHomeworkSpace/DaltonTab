b8dfcae2d7f2a57444d104898c3714d32eafe02c
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-22 15:03:46 -0500

    fix stuff

--- js/LoadSched.js
+++ js/LoadSched.js
@@ -2,60 +2,42 @@ $(document).ready(function() {
 	var schedulesUrl = "https://schedules.dalton.org/roux/index.php";
 	//$("#scheduleSignIn").click(function() {
 
-		var today = new Date();
-		var dd = today.getDate();
-		var mm = today.getMonth()+1; //January is 0!
-		var yyyy = today.getFullYear();
+	var start = moment();
+	if (start.day() == 0 || start.day() == 6) {
+		while (start.day() != 1) {
+			start.add(1, "day");
+		}
+	} else {
+		while (start.day() != 1) {
+			start.subtract(1, "day");
+		}
+	}
+	var end = moment().add(7, "days");
+	var dd = start.date();
+	var mm = start.month()+1; //January is 0!
+	var yyyy = start.year();
+	var startFormat = moment(start).format("YYYYMMDD");
+	var endFormat = moment(end).format("YYYYMMDD");
 
-		if(dd<10) {
-			dd='0'+dd
-		} 
+	if(dd<10) {
+		dd='0'+dd
+	} 
 
-		if(mm<10) {
-			mm='0'+mm
-		} 
+	if(mm<10) {
+		mm='0'+mm
+	} 
 
-		var today = yyyy+mm+dd;
-		
-		$.ajax({
-			url: schedulesUrl,
-			type: "POST",
-			data: {
-				rouxRequest: "<request><key></key><action>authenticate</action><credentials><username>" + daltonid + "</username><password type=\"plaintext\">" + password + "</password></credentials></request>"
-			},
-			success: function(data) {
-				console.log(data);
-				var $data = $(data);
-				var statusCode = $data.find("result").attr("status");
-				if (statusCode == 200) {
-					var key = $data.find("result").children("key").text();
-					var owner = $data.find("result").children("key").attr("owner");
-					//alert("Success! You've been signed in to Schedules.");
-					var id = key.split(":")[3]
-					var year = yyyy;
-					$.post(schedulesUrl, {rouxRequest: "<request><key>"+key+"</key><action>selectStudentCalendar</action><ID>" + id +"</ID><academicyear>" + year + "</academicyear><start>" + today + "</start><end>" + today + "</end></request>"}, function(response) {
-						console.log(response);
-						window.tempResponse = response;
-						console.log($(tempResponse).find("period"));
-					});
-					//window.location.reload();
-				} else {
-					// Uh oh.
-					var errCode = $data.find("error").children("code").text();
-					var errMsg = $data.find("error").children("message").text();
-					if (errCode == "505") {
-						//alert("That username and password combination didn't work.\n\nDouble-check you have't made any typos.");
-						$("#loginform").show();
-						$("#loggingin").hide();
-					} else {
-						alert("Error " + errCode + " - " + errMsg);
-					}
-				}
-			},
-			error: function() {
-				//alert("An error occured while connecting to Schedules.\n\nTry again later, or, if that doesn't work, send an email to emails@coursesplus.tk.");
-				window.close();
-			}
+	var today = yyyy+mm+dd;
+	
+	chrome.storage.sync.get(["schedulesLogin"], function(storage) {
+		var key = storage.schedulesLogin.key;
+		var owner = storage.schedulesLogin.username;
+		var id = key.split(":")[3];
+		var year = yyyy;
+		$.post(schedulesUrl, {rouxRequest: "<request><key>"+key+"</key><action>selectStudentCalendar</action><ID>" + id +"</ID><academicyear>" + year + "</academicyear><start>" + startFormat + "</start><end>" + endFormat + "</end></request>"}, function(response) {
+			var $data = $(response);
+			var periods = $data.find("period");
+			
 		});
-	//});
+	});
 });
