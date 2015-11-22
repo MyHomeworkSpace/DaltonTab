6584b0cf2a59ab7401922faf18eb13958a395fc9
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-22 12:43:31 -0500

    planhub api stuff

---
+++ js/planhubApi.js
@@ -0,0 +1,30 @@
+window.planhub = {
+	basePath: "https://planhub.me/api/v1/"
+};
+
+window.planhub.getNonce = function(callback) {
+	$.get(window.planhub.basePath + "csrf", function(data) {
+		callback(data.nonce);
+	});
+};
+
+window.planhub.get = function(url, callback) {
+	window.planhub.getNonce(function(nonce) {
+		$.get(window.planhub.basePath + url + "?nonce=" + nonce, function(data) {
+			if (typeof date == "string") {
+				callback({ status: "error", message: "Not signed in!" });
+				return;
+			}
+			callback(data);
+		});
+	});
+};
+
+window.planhub.post = function(url, data, callback) {
+	window.planhub.getNonce(function(nonce) {
+		data["nonce"] = nonce;
+		$.post(window.planhub.basePath + url, function(data) {
+			callback(data);
+		});
+	});
+};
