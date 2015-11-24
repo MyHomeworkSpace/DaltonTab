fd07af18b205b0362fde0f019196b8faacf667af
willweinstein <will@weinstein-nyc.com>
2015-11-23 22:24:10 -0500

    fixes double loadings
    all your base are belong to us

--- js/LoadSched.js
+++ js/LoadSched.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 			start.subtract(1, "day");
 		}
 	}
-	var end = moment().add(7, "days");
+	var end = moment().add(6, "days");
 	var dd = start.date();
 	var mm = start.month()+1; //January is 0!
 	var yyyy = start.year();
@@ -47,16 +47,13 @@ $(document).ready(function() {
 			var $data = $(response);
 			$data.find("period").each(function() {
 				var $item = $("<tr></tr>");
-
 				var name = $(this).children("section").children("name").text().replace("<![CDATA[", "").replace("]]>");
 				var instructor = $(this).children("instructor").children("name").text();
 				var location = $(this).children("location").text();
-
 				$item.append("<strong>" + name + " in " + location + "</strong><br />");
 				if (instructor != "") {
 					$item.append("with " + instructor);
 				}
-
 				$("[data-dow=" + moment($(this).children("date").text()).day() + "]").append($item);
 			});
 
