bd8810993b51763b6c1fe9b343d1ecca5f51c798
nywillb <nywillb@users.noreply.github.com>
2016-03-09 08:12:29 -0500

    Naaaaaammmmmmmmmeeeeeeee

--- js/sections.js
+++ js/sections.js
@@ -2,7 +2,7 @@ window.sections = {
     planhub: {
         name: "Homework",
         icon: "fa-file",
-        description: "View your homework information from PlanHub.",
+        description: "View your homework information from MyHomeworkSpace.",
         createHtml: function() {
 
         },
