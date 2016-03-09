de37d8e36c41c2326a77fef4047d8896d31ec12c
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-03-09 08:42:08 -0500

    yay name

--- js/mhsApi.js
+++ js/mhsApi.js
@@ -1,16 +1,16 @@
-window.planhub = {
+window.mhs = {
 	basePath: "https://planhub.me/api/v1/"
 };
 
-window.planhub.getNonce = function(callback) {
-	$.get(window.planhub.basePath + "csrf", function(data) {
+window.mhs.getNonce = function(callback) {
+	$.get(window.mhs.basePath + "csrf", function(data) {
 		callback(data.nonce);
 	});
 };
 
-window.planhub.get = function(url, callback) {
-	window.planhub.getNonce(function(nonce) {
-		$.get(window.planhub.basePath + url, { nonce: nonce }, function(data) {
+window.mhs.get = function(url, callback) {
+	window.mhs.getNonce(function(nonce) {
+		$.get(window.mhs.basePath + url, { nonce: nonce }, function(data) {
 			if (typeof data == "string") {
 				callback({ status: "error", message: "Not signed in!" });
 				return;
@@ -20,10 +20,10 @@ window.planhub.get = function(url, callback) {
 	});
 };
 
-window.planhub.post = function(url, data, callback) {
-	window.planhub.getNonce(function(nonce) {
+window.mhs.post = function(url, data, callback) {
+	window.mhs.getNonce(function(nonce) {
 		data["nonce"] = nonce;
-		$.post(window.planhub.basePath + url, function(data) {
+		$.post(window.mhs.basePath + url, function(data) {
 			callback(data);
 		});
 	});
