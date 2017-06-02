91084f1a1a5d1686773610ae83cbd2c8331171d4
Alex Studer <alex_studer@outlook.com>
2017-06-02 19:43:52 -0400

    add flag to analytics to say if it is developer version or not

--- js/analytics.js
+++ js/analytics.js
@@ -2,6 +2,7 @@ DaltonTab.Analytics = {
 	collectPingPayload: function(callback) {
 		chrome.storage.sync.get([ "sections", "schedulesLogin", "coursesToken" ], function(storage) {
 			callback({
+				prod: (chrome.runtime.id == "ggfjkmflbbjndabmnngilkfpmdegbfkm"),
 				sections: (storage.sections || "default"),
 				coursesLoggedIn: (storage.coursesToken ? true : false),
 				schedulesLoggedIn: (storage.schedulesLogin ? true : false)
