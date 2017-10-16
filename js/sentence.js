164ea2636589e069f76f7235fd2dcb77437e2ffc
William B <william@barkoffusa.com>
2017-10-16 09:56:15 -0400

    Wrong if statement order in sentence.js

--- js/sentence.js
+++ js/sentence.js
@@ -34,12 +34,12 @@ $(document).ready(function() {
                         })
                     } else if (assignmentsDone > 0) {
                         sentence = "Hi " + name + "! You have " + assignmentsDue + " assignments due " + nextDayName + ". " + assignmentsDone + " of those assignments have been completed.";
-                        sentenceContainer.text(sentence);
-                    } else if (assignmentsDone == assignmentsDue){
-                        sentence = "Hi " + name + "! You have completed all of your assignments due tomorrow. Congrats!";
                         sentenceContainer.text(sentence);                        
                     } else if (assignmentsDue == 0){
                         sentence = "Hi " + name + "! You don't have any assignments due tomorrow. Enjoy your free night!";
+                        sentenceContainer.text(sentence);
+                    } else if (assignmentsDone == assignmentsDue){
+                        sentence = "Hi " + name + "! You have completed all of your assignments due tomorrow. Congrats!";
                         sentenceContainer.text(sentence);
                     } else {
                         sentence = "Hi " + name + "! You have " + assignmentsDue + " assignments due " + nextDayName + ".";
