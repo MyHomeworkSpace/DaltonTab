416fdcd204773a22931cd562112558c58c538b12
Alex Studer <astuder@notetoscreen.com>
2018-01-23 21:17:05 -0500

    add option to build production, minified bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
+var webpack = require('webpack');
+
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 var path = require('path');
+
+var plugins = [
+	new ExtractTextPlugin("bundle.css"),
+	new HtmlWebpackPlugin({
+		title: "New tab"
+	}),
+	new CopyWebpackPlugin([
+		{
+			from: path.resolve(__dirname, "manifests", process.env.BROWSER + ".json"),
+			to: "manifest.json"
+		},
+		{ from: "img", to: "img" },
+		"bg.js"
+	])
+];
+
+if (process.env.NODE_ENV == "production") {
+	plugins.push(new UglifyJsPlugin({
+		uglifyOptions: {
+			ie8: false
+		}
+	}));
+	plugins.push(new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify('production')
+	}));
+}
 
 module.exports = {
 	entry: './app/main.js',
@@ -9,14 +39,14 @@ module.exports = {
 	devServer: {
 		historyApiFallback: {
 			rewrites: [
-				{ from: /.$/, to: '/newTab.html' },
+				{ from: /.$/, to: '/index.html' },
 			]
 		}
 	},
 
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build-' + process.env.BROWSER)
+		path: path.resolve(__dirname, (process.env.NODE_ENV == "production" ? 'prod-' : 'build-') + process.env.BROWSER)
 	},
 
 	module: {
@@ -53,20 +83,7 @@ module.exports = {
 		]
 	},
 
-	plugins: [
-		new ExtractTextPlugin("bundle.css"),
-		new HtmlWebpackPlugin({
-			title: "New tab"
-		}),
-		new CopyWebpackPlugin([
-			{
-				from: path.resolve(__dirname, "manifests", process.env.BROWSER + ".json"),
-				to: "manifest.json"
-			},
-			{ from: "img", to: "img" },
-			"bg.js"
-		])
-	],
+	plugins: plugins,
 
 	resolve: {
 		modules: [
