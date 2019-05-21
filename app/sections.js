bee25b40721feaffed0d2f23fa1ecfc435a619ba
Alex Studer <alex@studer.dev>
2019-05-21 18:54:25 -0400

    mark classes section as hidden so that it cannot be re-added

--- app/sections.js
+++ app/sections.js
@@ -31,7 +31,8 @@ export default {
 		description: "View and access your classes.",
 		background: "rgba(57, 146, 108, 0.9)",
 		storage: [],
-		component: Classes
+		component: Classes,
+		hidden: true
 	},
 	weather: {
 		name: "Weather",
