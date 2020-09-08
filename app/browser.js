fcf5403983664a25a09f08f2da6cd3b2331c6c42
William Barkoff <william@barkoffusa.com>
2020-09-08 12:43:53 -0400

    safari: begin work on support for Safari 14

--- app/browser.js
+++ app/browser.js
@@ -3,6 +3,8 @@ export function getBrowser() {
 		return "Edge";
 	} else if (navigator.userAgent.indexOf("Firefox") > -1) {
 		return "Firefox";
+	} else if (navigator.userAgent.indexOf("Safari") > -1) {
+		return "Safari";
 	}
 	return "Chrome";
 }
